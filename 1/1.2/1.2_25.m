a16b1a1c9e6b0138aa91e184edf8361a1d539354
Adithya Roy <adithya.roy38@gmail.com>
2026-07-07 15:21:38 +0530

    Change plot colors and add new function plot

--- 1/1.2/1.2_25.m
+++ 1/1.2/1.2_25.m
@@ -1,12 +1,15 @@
 syms x
 hold on;
 y=sin(x);
-fplot(y);
+fplot(y,'w');
 
 y1=x;
-fplot(y1);
+fplot(y1,'r');
 
 y2=x-(x.^3)/6;
-fplot(y2);
+fplot(y2,'b');
+
+y2=x-((x.^3)/6)+((x.^5)/120);
+fplot(y2,'y');
 
 hold off
