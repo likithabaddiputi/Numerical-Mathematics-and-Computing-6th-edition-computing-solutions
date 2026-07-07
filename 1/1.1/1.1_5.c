e1b16d2377bd6507697f950ffd2fdb3b9f1f8bfb
Likitha Baddiputi <likithabaddiputi@gmail.com>
2026-07-07 12:53:47 +0530

    Refactor output messages and verification logic

--- 1/1.1/1.1_5.c
+++ 1/1.1/1.1_5.c
@@ -19,6 +19,7 @@ int main(){
     double result=0;
     double dx=0.0000001;
     int n;
+    int flag =0;
     
     //Verifying relation
     for(n=1;n<4;n++)
@@ -30,26 +31,24 @@ int main(){
     }
     if (flag==3)
     {
-        printf("Relation Verified");
+        printf("Relation p(1)>p(2)>p(3)>p(4)>0 Verified\n");
 
     }
    
    
-    //Verifying p(n+1) = e−(n +1)p(n) 
+    printf("\nVerifying p(n+1) = e−(n +1)p(n)"); 
     for(int i=1;i<6;i++)
     {   
-        if(integration(i+1)==exp(1)-(i+1)*integration(i))
-        {
-            printf("Verified for N = %d",i);
-        }
+        double lhs = integration(i + 1);
+        double rhs = exp(1.0) - (i + 1) * integration(i);
+        if(fabs(lhs-rhs))
+            printf("\nVerified for N = %d",i);
     }   
         
     //printing first 20 elements
     for(int j=1;j<21;j++)
     {   
-        printf("The result for p(%d) is %lf\n",j,integration(j));
+        printf("\nThe result for p(%d) is %lf",j,integration(j));
     }
 
 }
-
-
