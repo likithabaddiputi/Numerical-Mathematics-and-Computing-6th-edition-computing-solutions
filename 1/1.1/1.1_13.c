f77fe9fbe449adfae996efee710145a8b440b4f2
Likitha Baddiputi <likithabaddiputi@gmail.com>
2026-07-07 15:00:03 +0530

    Implement __int128 support for factorial calculation
    Added a function to print __int128 values and updated factorial calculation to use __int128.

--- 1/1.1/1.1_13.c
+++ 1/1.1/1.1_13.c
@@ -1,5 +1,18 @@
+
 #include <stdio.h>
 #include<math.h>
+void print128(__int128 n) {
+    if (n == 0) { printf("0"); return; }
+    char str[50];
+    int i = 0;
+    while (n > 0) {
+        str[i++] = (char)(n % 10 + '0');
+        n /= 10;
+    }
+    while (i > 0) {
+        putchar(str[--i]);
+    }
+}
 
 int main()
 {
@@ -29,18 +42,21 @@ int main()
     }
     
     //using integer arithmetic
-    int resulta;
+    __int128 resultb;
     for(int i =1;i<31;i++)
     {   
-        resulta=1;
+        resultb=1;
         for(int j=1;j<=i;j++)
         {
-            resulta*=j;
+            resultb*=j;
         }
-        printf("Factorial of %d is %d\n",i,resulta);
+        printf("Factorial of %d is ",i);
+        print128(resultb);
+        printf("\n");
     }
     
     
 
 
 }
+ 
