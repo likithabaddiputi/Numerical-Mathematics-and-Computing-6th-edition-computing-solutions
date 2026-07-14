413185e439d8b70a5e51012541d6d461ce7614ed
Likitha Baddiputi <likithabaddiputi@gmail.com>
2026-07-14 14:24:34 +0530

    Create 2.2_24.c

---
+++ 2/2.2/2.2_24.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define pi 3.14159265358979323846
+int main()
+{
+    if((4/3)*pi == pi)
+        printf("4/3 evaluates to an integer, therefore (4/3)*pi = 1*pi = pi");
+    else
+        printf("4/3 does not evaluate to integer");
+}
