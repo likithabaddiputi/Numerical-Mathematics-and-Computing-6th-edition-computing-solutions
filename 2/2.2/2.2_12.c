0712033bf810dedf7bc8d497520f5c02333c1a8b
Likitha Baddiputi <likithabaddiputi@gmail.com>
2026-07-14 09:44:03 +0530

    Fix type casting and update main function for testing

--- 2/2.2/2.2_12.c
+++ 2/2.2/2.2_12.c
@@ -3,7 +3,7 @@
 #define ln2 log(2.0)
 double fn(double x)
 {
-    int m = x/ln2;
+    int m = (int)(x/ln2);
     double u = x- m*ln2;
     double u2 = u*u, u4 =u*u*u*u;
     double s = 2 + u2*((2520.0+28*u2)/(15120.0+420*u2+u4));
@@ -12,5 +12,10 @@ double fn(double x)
 }
 int main()
 {
-    fn();
+    double test[] = {0,1,-1,0.5,-0.123,-25.5,-1776,3.14159};
+    int len = sizeof(test)/sizeof(test[0]);
+    for(int i=0;i<len;++i)
+    {
+        printf("\t%lf\t\t%lf\t\t%lf\n",test[i],fn(test[i]),exp(test[i]));
+    }
 }
